825e57305a1f55bdc8e17045c17d760989fde9b3
2dcon <2jicon@gmail.com>
2025-05-01 04:48:28 +0800

    Update brook_setup.sh

--- brook_setup.sh
+++ brook_setup.sh
@@ -36,7 +36,7 @@ if test -f "$path/brook"; then
 		if [[ $YN == y ]]; then
 			systemctl stop brook
 			cd /temp/
-			wget $dlink
+			wget $dlink --no-check-certificate
 			cp -rf $fname /bin/brook/brook
 			chmod +x /bin/brook/brook
 			rm -f brook_linux_amd64
